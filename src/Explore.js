f7ac12ad93e03a620c89af73c81a124c28907bec
Huynh, Tu | RASIA <tu.huynh@rakuten.com>
2022-03-17 08:28:01 +0800

    Another bug bites the dust

--- src/Explore.js
+++ src/Explore.js
@@ -1,9 +1,12 @@
 import React, { useState, useRef, useMemo } from 'react'
 
+import PubSub from 'pubsub-js'
+
 import TinderCard from 'react-tinder-card'
 
 import Photo from './components/Photo'
 import Button from './components/Buttons'
+import Match from './Match'
 
 function shuffle(array) {
   let currentIndex = array.length,  randomIndex;
@@ -18,31 +21,31 @@ function shuffle(array) {
 
 const db = [
   {
-    name: 'Thao',
+    name: 'Hà',
     age: 'K15',
     bio: 'True love stories never have endings',
     image: 'https://scontent-xsp1-3.xx.fbcdn.net/v/t39.30808-6/217125181_3001527403425707_1832821722404312806_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=1gMzzkoZtzcAX-bEW8O&_nc_ht=scontent-xsp1-3.xx&oh=00_AT_zTE9PYJe9xmcZcrzwDHB1eAOnHn8L1W4F7P8cJ5vk3A&oe=6236D950'
   },
   {
-    name: 'Nguyen',
+    name: 'Nguyên',
     age: 'K14',
     bio: 'If you don\'t fight for what you want, don\'t cry for what you lost',
     image: 'https://images-ssl.gotinder.com/6225789fca044301007d251f/640x800_bf226470-baec-4ba2-b848-297292d068b1.jpg'
   },
   {
-    name: 'Uyen',
+    name: 'Nhi',
     age: 'K15',
     bio: 'Love all, trust a few, do wrong to none',
     image: 'https://images-ssl.gotinder.com/5e42b809f26ad90100e89933/640x800_9afb16d7-f18e-4b0b-9989-01fe65b61b33.jpg'
   },
   {
-    name: 'Hoang',
+    name: 'Hoàng',
     age: 'K15',
     bio: 'Thinking of you keeps me awake. Dreaming of you keeps me asleep. Being with you keeps me alive.',
     image: 'https://images-ssl.gotinder.com/5f941409ed2ff70100f71327/640x800_e877b848-53f9-4428-9e1b-3ffc863bf179.jpg'
   },
   {
-    name: 'Hoa',
+    name: 'Ly',
     age: 'K15',
     bio: 'There is never a time or place for true love. It happens accidentally, in a heartbeat, in a single flashing, throbbing moment',
     image: 'https://images-ssl.gotinder.com/5fcfaead95fef0010068946b/640x800_5ce9a7eb-bb7c-42bf-ba54-b12914e3831e.jpg'
@@ -76,6 +79,12 @@ export default function Explore() {
   // set last direction and decrease current index
   const swiped = (direction, nameToDelete, index) => {
     console.log(`${db[index].name} is swiped ${direction}`)
+
+    const match = Math.random() < 0.5
+    if ((direction === 'up' || direction === 'right') && match) {
+      PubSub.publish('match', { name: db[index].name, image: db[index].image })
+    }
+
     setLastDirection(direction)
     updateCurrentIndex(index - 1)
   }
@@ -132,6 +141,7 @@ export default function Explore() {
               swipeLeft={() => swipe('left')}
               swipeUp={() => swipe('up')}
               swipeRight={() => swipe('right')} />
+      <Match/>
     </React.Fragment>
   )
 }
---
+++ src/Match.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+
+import PubSub from 'pubsub-js'
+
+export default function Match() {
+  const [isOpen, setIsOpen] = useState(false)
+  const [name, setName] = useState('')
+  const [image, setImage] = useState('')
+
+  useEffect(() => {
+    PubSub.subscribe('match', (_, { name, image }) => {
+      setIsOpen(true)
+      setName(name)
+      setImage(image)
+    })
+  }, [])
+
+  return (
+    <React.Fragment>
+      {isOpen && <div className="match-screen" style={{ backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0) 20%, rgba(0,0,0,1)), url('${image}')` }}>
+        <div className="close" onClick={() => setIsOpen(false)}>
+          <svg focusable="false" aria-hidden="true" role="presentation" viewBox="0 0 24 24" width="20px" height="20px"
+               className="Sq(24px) P(4px)">
+            <path className=""
+                  d="M14.926 12.56v-1.14l5.282 5.288c1.056.977 1.056 2.441 0 3.499-.813 1.057-2.438 1.057-3.413 0L11.512 15h1.138l-5.363 5.125c-.975 1.058-2.438 1.058-3.495 0-1.056-.813-1.056-2.44 0-3.417l5.201-5.288v1.14L3.873 7.27c-1.137-.976-1.137-2.44 0-3.417a1.973 1.973 0 0 1 3.251 0l5.282 5.207H11.27l5.444-5.207c.975-1.139 2.438-1.139 3.413 0 1.057.814 1.057 2.44 0 3.417l-5.2 5.288z"/>
+          </svg>
+        </div>
+        <img src="./match.png" alt="Match"/>
+        <p>{name} cũng thích bạn!</p>
+      </div>}
+    </React.Fragment>
+  )
+}
