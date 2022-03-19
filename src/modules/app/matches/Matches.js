26362668c24542a6bcb934d3dfdf486a410a4f40
Huynh, Tu | RASIA <tu.huynh@rakuten.com>
2022-03-19 08:07:01 +0800

    this is Spartaaaaaaaa

--- src/modules/app/matches/Matches.js
+++ src/modules/app/matches/Matches.js
@@ -1,50 +1,74 @@
+import React, { useState } from 'react'
 import './Matches.scss'
-
-import { openProfile } from 'utils/openProfile'
 
 import { useSelector } from 'react-redux'
 import { selectListMatched } from 'modules/redux/matchedSlice'
+import Messages from './messages/Messages'
+
+import FUHCM from 'assets/img/fuhcm.jpg'
+
+const developer = {
+  name: 'Developer',
+  image: 'https://scontent-xsp1-3.xx.fbcdn.net/v/t1.6435-9/101371426_135019584843048_7604403132482404835_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=19026a&_nc_ohc=n9ektgA1V-kAX_k25g7&_nc_oc=AQmOprl-iQBVk2LCcBXko2qB4HELmEdf9ifNoSSTvyjeubswo0f5zkPBqULjuOjKQ9g&_nc_ht=scontent-xsp1-3.xx&oh=00_AT_3zVvKSWAAT7BEqu9dZyOEJHZse8e9sJv09xUJRlyejQ&oe=6258D7F2',
+  bio: 'I created this app',
+  age: 'K11'
+}
 
 function Matches() {
   const listMatched = useSelector(selectListMatched)
+  const [isMsgBoxOpen, setIsMsgBoxOpen] = useState(false)
+  const [profile, setProfile] = useState({})
+
+  function openChatBox(profile) {
+    setProfile(profile)
+    setIsMsgBoxOpen(true)
+  }
+
+  function closeChatBox() {
+    setProfile({})
+    setIsMsgBoxOpen(false)
+  }
 
   return (
-    <div className="matches-screen">
-      <div className="title">
-        New Matches
-      </div>
-      <div className="matches-list">
-        <div className="profile-card">
-          <div className="image"
-               style={{ backgroundImage: `url('https://i.chungta.vn/2020/09/07/photo-1-1578027492964533493668-4611-1599472131.jpg')` }}/>
-          <div className="name">FUHCM</div>
+    <React.Fragment>
+      <div className="matches-screen">
+        <div className="title">
+          New Matches
         </div>
-        {listMatched.map(e => (
-          <div key={e.id} className="profile-card" onClick={() => openProfile(e)}>
+        <div className="matches-list">
+          <div className="profile-card">
             <div className="image"
-                 style={{ backgroundImage: `url('${e.image}')` }}/>
-            <div className="name">{e.name}</div>
+                 style={{ backgroundImage: `url('${FUHCM}')` }}/>
+            <div className="name">FUHCM</div>
           </div>
-        ))}
-      </div>
-      <div className="title">
-        Messages
-      </div>
-      <div className="messages-list">
-        <div className="message-item">
-          <div className="image-wrapper">
-            <div className="image" style={{ backgroundImage: `url('https://d33wubrfki0l68.cloudfront.net/19e8b1005d45f56e2c10ad30e215298ce50c677e/6f09c/tu-huynh.jpg')` }}/>
-          </div>
-          <div className="details">
-            <div className="name">Developer</div>
-            <div className="last-message">
-              Welcome to the app!
+          {listMatched.map(e => (
+            <div key={e.id} className="profile-card" onClick={() => openChatBox(e)}>
+              <div className="image"
+                   style={{ backgroundImage: `url('${e.image}')` }}/>
+              <div className="name">{e.name}</div>
+            </div>
+          ))}
+        </div>
+        <div className="title">
+          Messages
+        </div>
+        <div className="messages-list">
+          <div className="message-item" onClick={() => openChatBox(developer)}>
+            <div className="image-wrapper">
+              <div className="image" style={{ backgroundImage: `url('${developer.image}')` }}/>
+            </div>
+            <div className="details">
+              <div className="name">Developer</div>
+              <div className="last-message">
+                Welcome to the app!
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      {isMsgBoxOpen && <Messages profile={profile} onClose={() => closeChatBox()}/>}
+    </React.Fragment>
   )
 }
 
-export default Matches
+export default React.memo(Matches)
