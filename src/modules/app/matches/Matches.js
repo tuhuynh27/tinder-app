cf76b8d93d283aa366d0d1ce230abbd6fd970ecb
Huynh, Tu | RASIA <tu.huynh@rakuten.com>
2022-03-23 07:29:02 +0800

    TDD: 1, Me: 0

--- src/modules/app/matches/Matches.js
+++ src/modules/app/matches/Matches.js
@@ -65,7 +65,9 @@ function Matches() {
             <div className="details">
               <div className="name">Developer</div>
               <div className="last-message">
-                Welcome to the app!
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+                The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
+                Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
               </div>
             </div>
           </div>
